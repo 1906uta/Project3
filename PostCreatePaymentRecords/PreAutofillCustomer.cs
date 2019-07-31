bf2d666d676230577d3353902f5b8ba163c8aa31
Djmendez5 <djemndez@dons.usfca.edu>
2019-07-31 10:51:38 -0500

    Added PreAutofill and PostAssignCase

---
+++ PostCreatePaymentRecords/PreAutofillCustomer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xrm.Sdk;
+using Microsoft.Xrm.Sdk.Query;
+using System.ServiceModel;
+namespace Plugin
+{
+    public class PreAutofillCustomer : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            // Obtain the tracing service
+            ITracingService tracingService =
+            (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            // Obtain the execution context from the service provider.  
+            IPluginExecutionContext context = (IPluginExecutionContext)
+                serviceProvider.GetService(typeof(IPluginExecutionContext));
+
+            // The InputParameters collection contains all the data passed in the message request.  
+            if (context.InputParameters.Contains("Target") &&
+                context.InputParameters["Target"] is Entity)
+            {
+                // Obtain the target entity from the input parameters.  
+                Entity incident = (Entity)context.InputParameters["Target"];
+
+                // Obtain the organization service reference which you will need for  
+                // web service calls.  
+                IOrganizationServiceFactory serviceFactory =
+                    (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+                IOrganizationService service = serviceFactory.CreateOrganizationService(context.UserId);
+
+                try
+                {
+                    if (incident.Attributes.Contains("new_mortgage"))
+                    {
+                        //get info
+                        EntityReference mortgage = (EntityReference)incident.Attributes["new_mortgage"];
+                        Entity entity = service.Retrieve(mortgage.LogicalName, mortgage.Id,
+                     new ColumnSet("new_country", "new_contact"));
+
+                       //assign it 
+                        incident.Attributes.Add("customerid", entity.Attributes["new_contact"]);
+                    
+                       
+                    }
+                }
+                catch (FaultException<OrganizationServiceFault> ex)
+                {
+                    throw new InvalidPluginExecutionException("An error occurred in FollowUpPlugin.", ex);
+                }
+
+                catch (Exception ex)
+                {
+                    tracingService.Trace("FollowUpPlugin: {0}", ex.ToString());
+                    throw;
+                }
+            }
+        }
+
+
+    }
+
+
+
+}
